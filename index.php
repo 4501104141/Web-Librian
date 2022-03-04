151a810067190b63973fd209430b3749fdca5683
NhatMinh <4501104141@student.hcmue.edu.vn>
2022-03-04 20:27:03 +0700

    Dang lam phan login

--- index.php
+++ index.php
@@ -24,8 +24,14 @@ echo "Connected successfully";
 <!--
     connect DB
 -->
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
 
 </body>
 </html>
