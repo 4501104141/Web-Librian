7d0d6f1f9b9b19781d9696d12ed2f1da9bc873b2
NhatMinh <4501104141@student.hcmue.edu.vn>
2022-03-10 15:09:58 +0700

    Research show DB

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
 <head>
-<title>Page Title</title>
-</head>
-<body>
-<!--
-    connect DB
--->
-<?php
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css">
+
+  <!-- Connect DB -->
+  <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -19,19 +20,62 @@ $conn = new mysqli($servername, $username, $password, "quanlythuvien");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
 ?>
-<!--
-    connect DB
--->
+<!-- Connect DB -->
 
-<form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname"><br><br>
-  <input type="submit" value="Submit">
+<!-- Insert DB -->
+<!-- Insert DB -->
+
+<!-- Show DB -->
+  <?php
+  $sql = "SELECT MaSach,	TenSach, TacGia,	NhaXuatBan,	NgayPhatHanh FROM sach";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["MaSach"]. " - Name: " . $row["TenSach"]. " " . $row["TacGia"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  ?>
+<!-- Show DB -->
+</head>
+<body>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
+<!-- Check account -->
+  <?php 
+    $sql = "SELECT MaSach,	TenSach, TacGia,	NhaXuatBan,	NgayPhatHanh FROM sach";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        
+          echo "id: " . $row["MaSach"]. " - Name: " . $row["TenSach"]. " " . $row["TacGia"]. "<br>";
+        
+      }
+    } else {
+      echo "0 results";
+    }
+  ?>
+<!-- Check account -->
+  <script src="bootstrap-5.1.3-dist/js/bootstrap.js"></script>
 </body>
 </html>
